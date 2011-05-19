427fbbddf6442913e7b70d9711232284200f5db5
Jayesh Salvi <jayesh@3dtin.com>
2011-05-19 10:07:02 +0530

    Python backend that opens native libcsgop.so using ctypes
    This commit includes the `cadmium` module, which includes a backend that
    interfaces with the native libcsgop.so using ctypes. It also defines
    several datatypes that are used by python code to pass data back-n-forth
    with the native backend. It also opens libc.so and defines a reference
    to free(), which is used by python code to free the memory allocated by
    the libcsgop.so.

---
+++ src/cadmium/backend.py
@@ -0,0 +1,39 @@
+
+from ctypes import *
+
+libc = cdll.LoadLibrary('/lib/libc.so.6')
+libcsg = cdll.LoadLibrary('../build/libcsgop.so')
+
+csgop = libcsg.csgop
+memfree = libc.free
+ERR_NOT_SIMPLE = libcsg.ERR_NOT_SIMPLE
+OP_UNION = libcsg.OP_UNION
+OP_INTERSECTION = libcsg.OP_INTERSECTION
+OP_SUBTRACTION = libcsg.OP_SUBTRACTION
+
+PDOUBLE = POINTER(c_double)
+PPDOUBLE = POINTER(PDOUBLE)
+PINT = POINTER(c_int)
+PPINT = POINTER(PINT)
+DBLARR = c_double * 3
+INTARR = c_int * 3
+
+# Function prototype
+csgop.argtypes = [ 
+  PPDOUBLE, c_int, c_int, PPINT, c_int, c_int,
+  PPDOUBLE, c_int, c_int, PPINT, c_int, c_int, c_int ]
+
+# Function return data type
+PPDOUBLE = POINTER(POINTER(c_double))
+PPINT = POINTER(POINTER(c_int))
+
+class PolyPack(Structure):
+  _fields_ = [
+              ('error', c_int),
+              ('num_vertices', c_int),
+              ('num_faces', c_int),
+              ('vertices', PPDOUBLE),
+              ('faces', PPINT),
+              ]
+
+csgop.restype = POINTER(PolyPack)
