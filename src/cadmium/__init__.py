8c40814b647bfb8d20946b278ec58501f32571ef
Jayesh Salvi <jayesh@3dtin.com>
2011-05-19 16:32:34 +0530

    Export primitive objects from top level cadmium package

--- src/cadmium/__init__.py
+++ src/cadmium/__init__.py
@@ -1,2 +1,10 @@
 
+import polyhedron
+import primitives.cylinder
+import primitives.sphere
+import primitives.box
+
+Cylinder = primitives.cylinder.Cylinder
+Sphere = primitives.sphere.Sphere
+Box = primitives.box.Box
 
