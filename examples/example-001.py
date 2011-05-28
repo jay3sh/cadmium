30868975aa1f152b4646c9db0af317aa916f3dd8
Jayesh Salvi <jayesh@3dtin.com>
2011-05-28 12:09:35 +0530

    Create an interesting shape for first example

--- examples/example-001.py
+++ examples/example-001.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys
+import math
 sys.path.append('./src')
 
 from cadmium import Box
@@ -9,17 +10,16 @@ from cadmium import Cylinder
 
 stlfname = sys.argv[1]
 
-b1 = Box(x=4,y=4,z=4)
-b2 = Box(x=4,y=4,z=4)
-b2.translate([2,2,2])
+c0 = Cylinder(radius=1, height=8)
+c1 = Cylinder(radius=1, height=6)
+c1.rotate([0,0,1], 30*math.pi/180.0)
+c2 = Cylinder(radius=1, height=6)
+c2.rotate([0,0,1], -30*math.pi/180.0)
 
-s1 = Sphere(radius=3)
-s1.translate([2,3,2])
+b = Box(x=1,y=1,z=8)
+b.translate([0.5,0.5,0])
 
-c1 = Cylinder(radius=1, height=8)
-c1.translate([1,1,2])
-
-u = b1*c1
+u = (c0 + c1 + c2) - b
 
 fd = open(stlfname, 'w')
 fd.write(u.toSTL())
