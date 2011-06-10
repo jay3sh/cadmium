8a495fbca4c3430ea03b0384c63de6a1763fe0bb
Jayesh Salvi <jayesh@3dtin.com>
2011-06-10 18:53:06 +0530

    Auto center option for box and cylinder

--- src/cadmium/primitives/boxOCC.py
+++ src/cadmium/primitives/boxOCC.py
@@ -9,7 +9,8 @@ from cadmium.polyhedronOCC import Polyhedron
 
 class Box(Polyhedron):
   
-  def __init__(self, x=10, y=10, z=10):
+  def __init__(self, x=10, y=10, z=10, center=False):
     self.instance = BRepPrimAPI_MakeBox(x,y,z)
     Polyhedron.__init__(self, shape=self.instance.Shape())
+    if center: self.translate(-x/2.0, -y/2.0, -z/2.0)
     
--- src/cadmium/primitives/cylinderOCC.py
+++ src/cadmium/primitives/cylinderOCC.py
@@ -10,8 +10,9 @@ from cadmium.polyhedronOCC import Polyhedron
 
 class Cylinder(Polyhedron):
   
-  def __init__(self, radius=5, height=10, pie=360):
+  def __init__(self, radius=5, height=10, pie=360, center=False):
     self.instance = BRepPrimAPI_MakeCylinder(
       radius, height, pie*math.pi/180)
     Polyhedron.__init__(self, shape=self.instance.Shape())
+    if center: self.translate(0,0,-height/2.0)
     
