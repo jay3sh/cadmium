2f1fad5db99c0d0c3a88f08b3848e98491b3d56c
Jayesh Salvi <jayesh@3dtin.com>
2011-06-11 10:36:39 +0530

    Update example-003 to generate interesting shape

--- examples/example-003.py
+++ examples/example-003.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python
 
-#
-# This is a failing examples.
-# It is found to consume lot of CPU and occupy lot of memory
-# leading to a system crash.
-#
-
 import sys
 import math
 sys.path.append('./src')
@@ -14,9 +8,8 @@ from cadmium import *
 
 stlfname = sys.argv[1]
 
-b0 = Box(x=2,y=2,z=2)
-
-s0 = Sphere(radius=2)
+b0 = Box(x=4,y=4,z=4, center=True)
+s0 = Sphere(radius=2.5)
 
 u = s0 - b0
 
