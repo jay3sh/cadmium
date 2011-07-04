d5359ce9d551d694e02349384a2307927a9c0788
Jayesh Salvi <jayesh@3dtin.com>
2011-07-04 18:53:22 +0530

    Add center flag for Wedge and Sphere
    For Sphere it's only for the sake of consistency. It's centered by
    default.

--- src/cadmium/primitives/wedge.py
+++ src/cadmium/primitives/wedge.py
@@ -10,7 +10,14 @@ from cadmium.solid import Solid
 
 class Wedge(Solid):
   
-  def __init__(self, dx=5, dy=5, dz=5, ltx=0):
+  def __init__(self, dx=5, dy=5, dz=5, center=True, ltx=0):
+    if center:
+      self.centerTranslation = (0,-dy/2.0,0)
+    else:
+      self.centerTranslation = (0,0,0)
+
     self.instance = BRepPrimAPI_MakeWedge(dx, dy, dz, ltx)
     Solid.__init__(self, self.instance.Shape())
+
+    self.translate(delta=self.centerTranslation)
     
