14a9d99fea80950f1ce5ae09e88dafeafd5fe568
Jayesh Salvi <jayesh@3dtin.com>
2011-11-27 08:30:43 +0530

    Fix the non-uniform scaling problem example

--- examples/pythonocc/scale-problem.py
+++ examples/pythonocc/scale-problem.py
@@ -1,12 +1,14 @@
 
 from OCC.BRepPrimAPI import BRepPrimAPI_MakeCylinder
 from OCC.BRepBuilderAPI import BRepBuilderAPI_GTransform
-from OCC.gp import gp_GTrsf, gp_Trsf
+from OCC.gp import gp_GTrsf, gp_Trsf, gp_Mat
 from OCC.Precision import Precision_Angular, Precision_Confusion
 from OCC import StlAPI
 
 shape = BRepPrimAPI_MakeCylinder(20,40).Shape()
 
+# This options creates unwanted polygons
+'''
 xform = gp_Trsf()
 xform.SetValues(
   1.5, 0, 0, 0,
@@ -15,6 +17,16 @@ xform.SetValues(
   Precision_Angular(), Precision_Confusion()
 );
 brep = BRepBuilderAPI_GTransform(shape, gp_GTrsf(xform), False)
+'''
+
+# This options works as desired
+xform = gp_GTrsf()
+xform.SetVectorialPart(gp_Mat(
+  1.5, 0, 0,
+  0, 1, 0,
+  0, 0, 1,
+))
+brep = BRepBuilderAPI_GTransform(shape, xform, False)
 brep.Build()
 shape = brep.Shape()
 
